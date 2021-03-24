5249bb1011b8306ea65784bbc4a2cd77832db29e
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-03-24 14:16:55 +0300

    Main Metodu incelendi

--- DegiskenlerFrm/Form1.Designer.cs
+++ DegiskenlerFrm/Form1.Designer.cs
@@ -50,7 +50,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(54, 105);
+            this.button1.Location = new System.Drawing.Point(54, 96);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 2;
