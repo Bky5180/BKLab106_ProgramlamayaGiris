c8367187ca3e48bb2c8d4655f83d8b71a3bc4deb
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-03-24 13:30:50 +0300

    TryCatch windows Ekrani

---
+++ TryCatchWindows/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TryCatchWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFinallyOnayla = new System.Windows.Forms.Button();
+            this.btnDetayliOnayla = new System.Windows.Forms.Button();
+            this.btnHataYonetimliOnayla = new System.Windows.Forms.Button();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGirisAlani = new System.Windows.Forms.TextBox();
+            this.btnHataTipleri = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFinallyOnayla
+            // 
+            this.btnFinallyOnayla.Location = new System.Drawing.Point(17, 268);
+            this.btnFinallyOnayla.Name = "btnFinallyOnayla";
+            this.btnFinallyOnayla.Size = new System.Drawing.Size(502, 56);
+            this.btnFinallyOnayla.TabIndex = 11;
+            this.btnFinallyOnayla.Text = "ONAYLA - HATA YÖNETİMLİ (FINALLY BLOĞU)";
+            this.btnFinallyOnayla.UseVisualStyleBackColor = true;
+            this.btnFinallyOnayla.Click += new System.EventHandler(this.btnFinallyOnayla_Click);
+            // 
+            // btnDetayliOnayla
+            // 
+            this.btnDetayliOnayla.Location = new System.Drawing.Point(17, 205);
+            this.btnDetayliOnayla.Name = "btnDetayliOnayla";
+            this.btnDetayliOnayla.Size = new System.Drawing.Size(502, 57);
+            this.btnDetayliOnayla.TabIndex = 10;
+            this.btnDetayliOnayla.Text = "ONAYLA - HATA YÖNETİMLİ (SİSTEM MESAJI)";
+            this.btnDetayliOnayla.UseVisualStyleBackColor = true;
+            this.btnDetayliOnayla.Click += new System.EventHandler(this.btnDetayliOnayla_Click);
+            // 
+            // btnHataYonetimliOnayla
+            // 
+            this.btnHataYonetimliOnayla.Location = new System.Drawing.Point(17, 142);
+            this.btnHataYonetimliOnayla.Name = "btnHataYonetimliOnayla";
+            this.btnHataYonetimliOnayla.Size = new System.Drawing.Size(502, 57);
+            this.btnHataYonetimliOnayla.TabIndex = 9;
+            this.btnHataYonetimliOnayla.Text = "ONAYLA - HATA YÖNETİMLİ";
+            this.btnHataYonetimliOnayla.UseVisualStyleBackColor = true;
+            this.btnHataYonetimliOnayla.Click += new System.EventHandler(this.btnHataYonetimliOnayla_Click);
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.Location = new System.Drawing.Point(17, 79);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(502, 57);
+            this.btnOnayla.TabIndex = 8;
+            this.btnOnayla.Text = "ONAYLA - HATA YÖNETİMSİZ";
+            this.btnOnayla.UseVisualStyleBackColor = true;
+            this.btnOnayla.Click += new System.EventHandler(this.btnOnayla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lütfen telefon numaranızı giriniz:";
+            // 
+            // txtGirisAlani
+            // 
+            this.txtGirisAlani.Location = new System.Drawing.Point(17, 43);
+            this.txtGirisAlani.Name = "txtGirisAlani";
+            this.txtGirisAlani.Size = new System.Drawing.Size(502, 30);
+            this.txtGirisAlani.TabIndex = 6;
+            // 
+            // btnHataTipleri
+            // 
+            this.btnHataTipleri.Location = new System.Drawing.Point(17, 330);
+            this.btnHataTipleri.Name = "btnHataTipleri";
+            this.btnHataTipleri.Size = new System.Drawing.Size(502, 56);
+            this.btnHataTipleri.TabIndex = 12;
+            this.btnHataTipleri.Text = "Hata Tipleri";
+            this.btnHataTipleri.UseVisualStyleBackColor = true;
+            this.btnHataTipleri.Click += new System.EventHandler(this.btnHataTipleri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 400);
+            this.Controls.Add(this.btnHataTipleri);
+            this.Controls.Add(this.btnFinallyOnayla);
+            this.Controls.Add(this.btnDetayliOnayla);
+            this.Controls.Add(this.btnHataYonetimliOnayla);
+            this.Controls.Add(this.btnOnayla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGirisAlani);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFinallyOnayla;
+        private System.Windows.Forms.Button btnDetayliOnayla;
+        private System.Windows.Forms.Button btnHataYonetimliOnayla;
+        private System.Windows.Forms.Button btnOnayla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGirisAlani;
+        private System.Windows.Forms.Button btnHataTipleri;
+    }
+}
+
---
+++ TryCatchWindows/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TryCatchWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+             * ISTISNA TIPLERI
+             * 
+             * Calisma Zamani Hataları (Runtime Exception) => Cogunlukla kullanici kaynakli hatalardir. (Verinin yanlis, eksik ya da bos girilmesi). Cozum Yolu, try-catch ile hatalari ele alabilirsiniz...
+             * 
+             * Derleme Zamani Hatalari (Compile Exception) => Tamamen yazilimci kaynakli hatalardir. Yazim hatalari meydana geldiginde devreye girer. En kolay cozulen hata tipidir. Visual Studio sizi uyarir (asagidaki Error List penceresi ile). Ilgili hataya cift tiklayip detaya gidebilir ve hatayi aninda duzeltebilirsiniz...
+             * 
+             * Mantiksal Hatalar => Tamamen programci kaynakli hatalardir. Uygulamanin algoritmasinda meydana gelir. Ornegin bolme yapilacak yerde islem unutulmasi, degerin yanlis bir degerle degistirilmesi... Cozulmesi en zor hatalardir. Anlik olarak derleme asamasini takip etmeli ve degerlerinizi gozlemlemelisiniz. Bu konuda Visual Studio size sagladigi tool'larla yardimci olur. (Immediate Window, Watch, Breakpoint)
+             */
+
+        /*
+         * BREAKPOINT => Derleme asamasina adim adim dahil olmak icin kullandigimiz yardimci tool'dur. Yapmaniz gereken tek sey, hangi satirdan derleme anina dahil olmak istiyorsaniz, ilgili satirin en sol kismina gelip (gri alan) mouse'un sol tusuyla bir kırmızı top yerlestirmek. Artik derleyici bu satira geldigi zaman sizi olaya dahil edecektir...
+         * F11 ile adim ilerleyebilirsiniz. Degiskenlerin, nesnelerin uzerine gelip o anki degerlerini ogrenebilirsiniz, islemin bittigine inanıyorsaniz F5'e basip breakpoint'ten cikabilir ve islemin kendisini devam ettirmesini saglayabilirsiniz.
+         * Sol blokta cikan sari ok, derlenen satirin isaretcisidir. Mouse yardimiyla ileri ya da geri oynatabilir yani adimlari atlayabilir ya da tekrarlatabilirsiniz...
+         */
+
+        /*
+         * WATCH
+         * Yalnizca run-time'da - calisma zamani - kullanabilecegimiz bir tool'dur. Dolayisiyla pencereyi sadece calisma zamaninda gorursunuz...
+         * İlk ayarlarinizda Watch penceresi kapali olabilir. Uygulamanizi calistirip "CTRL+ALT+W" ardindan 1'e basarsaniz Watch penceresini getirirsiniz...
+         * Run-time'de elinizde olan degerleri (kontrol degerler, degisken degerleri) degistirmenize, izlemenize olanak saglayan yardimci tool'dur. Acilan penceredeki "Name" alanina ilgili degiskenin adini yazip Enter'a basarsaniz, "Value" alaninda o anki degerini gozlemleyebilirsiniz. Hatta degistirebilirsiniz...
+         * "Name" alanina degeri ilk yazmaya basladiginizda intellisense size destek vermez. Ancak "CTRL+Space" kombinasyonu ile intellisense'i aktif hale getirebilirsiniz. Hatta cogu zaman kendisi otomatik olarak tamamlar. 
+         * Noktalar, virgüller, noktali virgüller - yani tum yazim kurallari - watch penceresi icin de gecerlidir...
+         */
+
+        //Eger disaridan bir veri aliyorsaniz, verinin tutarligindan emin de degilseniz, orada mutlaka hata riski vardır!
+
+        private void btnOnayla_Click(object sender, EventArgs e)
+        {
+            int gelenDeger = Convert.ToInt32(txtGirisAlani.Text);
+            MessageBox.Show("Tebrikler! Doğru telefon formatı girdiniz!");
+        }
+
+        private void btnHataYonetimliOnayla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Bu alana hata riski olan kodlar yazilir...
+                int gelen = Convert.ToInt32(txtGirisAlani.Text);
+                MessageBox.Show("Tebrikler! Doğru telefon formatı girdiniz!");
+            }
+            catch
+            {
+                //Try blogu icerisine yazmis oldugunuz kodlarda herhangi bir hata meydana gelirse, catch blogu devreye girer...
+                MessageBox.Show("Telefon numarası yazmayı bile beceremedin!");
+            }
+        }
+
+        private void btnDetayliOnayla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int gelen = Convert.ToInt32(txtGirisAlani.Text);
+                MessageBox.Show("Tebrikler! Doğru formatı girdiniz!");
+            }
+            catch (Exception hata)
+            {
+                //catch ifadesinin yanindaki "Exception"ın kelime anlami => Istisna..
+                //Uygulamanizin kararligini bozan istisnai durumla ilgili size detaylari teslim eden nesnedir...
+                MessageBox.Show(hata.Message);  //Hata'nın sistem tarafindan atanan mesajini size teslim eder...
+            }
+        }
+
+        private void btnFinallyOnayla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int gelen = Convert.ToInt32(txtGirisAlani.Text);
+                MessageBox.Show("Tebrikler!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Hata olsa da olmasa da calismasini istediginiz kodlari yazdiginiz alandir... Try-Catch-Finally uclusunde en son calisir...
+                MessageBox.Show("Hata var mı yok mu bilmiyorum ama çalıştım!");
+            }
+        }
+
+        private void btnHataTipleri_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int gelen = Convert.ToInt32(txtGirisAlani.Text);
+            }
+            catch (FormatException ex) // format hata tipi
+            {
+                MessageBox.Show("FormatException : " + ex.Message);
+            }
+            catch (DivideByZeroException ex) // sıfıra bölünme hata tipi
+            {
+                MessageBox.Show("DivideByZeroException : " + ex.Message);
+            }
+            catch (OverflowException ex) // veri tipinin boyutunu aşması durumunda alınan hata tipi
+            {
+                MessageBox.Show("OverflowException : " + ex.Message);
+            }
+            catch(Exception ex) // exception sınıfı tanımlı olan hatalar haricinde bir hata ile karşılaşılır ise, bu alandaki hata bloğu çalışacaktır
+            {
+                MessageBox.Show("Exception : " + ex.Message);
+            }
+        }
+    }
+}
