fbce6057ba0711598c5f9f33d85d28c5a27696b9
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-03-25 13:35:33 +0300

    ifElse Reyon Kontrol

--- ifElse/Form2.cs
+++ ifElse/Form2.cs
@@ -51,16 +51,69 @@ namespace ifElse
              * 
              */
 
-            string not1 = txtBirinciDeger.Text;
-            string not2 = txtikinciDeger.Text;
+            int not1 = int.Parse(txtBirinciDeger.Text);
+            int not2 = int.Parse(txtikinciDeger.Text);
+            int ortalama = (not1 + not2) / 2;
+            if (ortalama >= 0 && ortalama < 30)
+            {
+                MessageBox.Show("FF");
+            }
+            else if (ortalama >= 30 && ortalama < 50)
+            {
+                MessageBox.Show("DD");
+            }
+            else if (ortalama >= 50 && ortalama < 70)
+            {
+                MessageBox.Show("BB");
+            }
+            else if (ortalama >= 70 && ortalama <= 100)
+            {
+                MessageBox.Show("AA");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış not aralığı");
+            }
 
-
+            
         }
+
+        //Disaridan urun adi girilecek, kasiyer bize urunun hangi reyonda oldugunu soyleyecek...
+        //Domates, Biber, Patlican => Sebze Reyonu
+        //Diş Macunu, Parfüm, Şampuan => Kozmetik Reyonu
+        //Cep Telefonu, Bilgisayar, Ses Sistemi => Teknoloji Reyonu
+        //Başka bir ürün girilirse "Bu ürün bizde yok!" uyarisi verilsin!
         private void Form2_Load(object sender, EventArgs e)
         {
 
         }
 
-      
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Disaridan urun adi girilecek, kasiyer bize urunun hangi reyonda oldugunu soyleyecek...
+            //Domates, Biber, Patlican => Sebze Reyonu
+            //Diş Macunu, Parfüm, Şampuan => Kozmetik Reyonu
+            //Cep Telefonu, Bilgisayar, Ses Sistemi => Teknoloji Reyonu
+            //Başka bir ürün girilirse "Bu ürün bizde yok!" uyarisi verilsin!
+
+            string mesaj = "";
+            if (txtBirinciDeger.Text == "Domates" || txtBirinciDeger.Text == "Biber" || txtBirinciDeger.Text == "Patlıcan")
+            {
+                mesaj = "Sebze reyonuna buyrun!";
+            }
+            else if (txtBirinciDeger.Text == "Diş Macunu" || txtBirinciDeger.Text == "Parfüm" || txtBirinciDeger.Text == "Şampuan")
+            {
+                mesaj = "Kozmetik reyonuna lütfen";
+            }
+            else if (txtBirinciDeger.Text == "Cep Telefonu" || txtBirinciDeger.Text == "Bilgisayar" || txtBirinciDeger.Text == "Ses Sistemi")
+            {
+                mesaj = "Teknoloji reyonunda bulabilirsiniz!";
+            }
+            else
+            {
+                mesaj = "Bu ürün karşıki markette!";
+            }
+            MessageBox.Show(mesaj);
+        }
     }
 }
