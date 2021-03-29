dafcf159e427253e436c6b196eb37b575d55938e
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-03-29 14:19:14 +0300

    For Dongusune devam

--- Loop_For/Form1.cs
+++ Loop_For/Form1.cs
@@ -47,6 +47,7 @@ namespace Loop_For
 
         private void button3_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
             for (char c = 'A'; c <= 'Z'; c++)
             {
                 listBox1.Items.Add(c + "-" + (int)c);
@@ -56,12 +57,43 @@ namespace Loop_For
         private void button4_Click(object sender, EventArgs e)
         {
             // 1-100 arasindaki cift sayilarin toplami ile tek sayilarin toplaminin farklarinin karesi kactir?
+            // camel case 
+            int ciftToplam =0, tekToplam=0;
+            
+            // Pascal case
+            int fark=0, TekKare;
 
+            // snack case 
+            int cift_Toplam_sayilar; 
+            int tek_Toplam_;
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                // eger Tel satirlik komut varsa scope acmaya gerek yoktur
+                if (i % 2 == 0)
+                    ciftToplam += i;
+                else
+                    tekToplam += i;
+            }
+            fark = (ciftToplam - tekToplam) * (ciftToplam - tekToplam);
+            MessageBox.Show((fark).ToString());
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             // 1945 gunumuze kadar yillari listbox'a ekleyelim. Ancak 2020 ve 1995 yilini eklemek istemiyorum.
+            listBox1.Items.Clear();
+            int year = DateTime.Now.Year;
+            for (int i = 1945; i <= year; i++)
+            {
+                if (i != 1995 && i != 2020)
+                    listBox1.Items.Add(i);
+                //if (i == 1995 &&  i == 2020)
+                //    continue;
+
+                listBox1.Items.Add(i);
+            }
 
         }
     }
