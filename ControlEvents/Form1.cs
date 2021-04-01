82f04bb12b3f6bed3d7b20c91191be9c3c2d5efc
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-04-01 13:55:30 +0300

    String Metodlar

---
+++ ControlEvents/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            //Mouse alana girdigi anda, textbox'un arkaplan ve yazi rengi degissin...
+            textBox1.BackColor = Color.Maroon;
+            textBox1.ForeColor = Color.Orange;
+           
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ulong yazilan = (ulong)textBox2.Text.Length;
+            label2.Text = yazilan.ToString();
+
+        }
+    }
+}
