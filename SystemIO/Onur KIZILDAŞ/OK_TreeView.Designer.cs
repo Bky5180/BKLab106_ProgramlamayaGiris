37cb9f00cbb7246d6b42dfefb995ce254fb92c13
Onur KIZILDAŞ <onurkizildas_1987@hotmail.com>
2021-04-12 00:26:38 +0300

    TreeView form dosyası eklendi

---
+++ SystemIO/Onur KIZILDAŞ/OK_TreeView.Designer.cs
@@ -0,0 +1,40 @@
+﻿
+namespace SystemIO.Onur_KIZILDAŞ
+{
+    partial class OK_TreeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "OK_TreeView";
+        }
+
+        #endregion
+    }
+}
